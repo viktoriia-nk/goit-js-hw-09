dfb07de35e342605c69ae552c7710e1bfe0f5167
Viktoriia Nistorika <vnistorika@gmail.com>
2022-07-06 15:27:54 +0300

    заканчивает

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -31,16 +31,20 @@ const options = {
     },
   };
 
+  const addLeadingZero = value => {
+    return String(value).padStart(2, '0');
+  };
+
   function convertMs(ms) {
       const second = 1000;
       const minute = second * 60;
       const hour = minute * 60;
       const day = hour * 24;
     
-      const days = Math.floor(ms / day);
-      const hours = Math.floor((ms % day) / hour);
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+      const days = addLeadingZero(Math.floor(ms / day));
+      const hours = addLeadingZero(Math.floor((ms % day) / hour));
+      const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+      const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
       return { days, hours, minutes, seconds };
     }
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form')
+const delayEl = formEl.querySelector('[name = "delay"]')
+const stepEl = formEl.querySelector('[name="step"]')
+const amountEl = formEl.querySelector('[name="amount"]')
+
+let delayValue
+let stepValue 
+let amountValue 
+let amountArr = []
+let position = 0
+
+
+const submitFn = event => {
+  event.preventDefault()
+
+  delayValue = Number(delayEl.value)
+  stepValue = Number(stepEl.value)
+  amountValue = Number(amountEl.value)
+  // console.log(typeof delayValue);
+  // console.log(stepValue);
+  // console.log(amountValue);
+  for (let i = 1; i<=amountValue; i+=1){
+    amountArr.push(i)
   }
+  amountArr.map((num)=>{
+    position = num
+    delayValue = position === 1 ? delayValue : (delayValue += stepValue);
+    createPromise(position, delayValue)
+    .then(({position, delayValue})=>{
+      Notiflix.Notify.success(`🥳 Fulfilled promise ${position} in ${delayValue}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delayValue}ms`);
+    })
+    .catch(({position, delayValue})=>{
+      Notiflix.Notify.failure(`💥 Rejected promise ${position} in ${delayValue}ms`)
+      console.log(`❌ Rejected promise ${position} in ${delayValue}ms`)
+    })
+  })
+  formEl.reset()
+
+}
+
+formEl.addEventListener("submit", submitFn)
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout (() => {
+    if (shouldResolve) {
+      resolve ({position, delay});
+    } else {
+      reject ({position, delay});
+    };
+  }, delay)
+  
+});
+
 }
